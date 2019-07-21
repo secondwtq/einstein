3695e138a5d5bf6eaa5ac611e2a1fc79926f7827
secondwtq <lovejay-lovemusic@outlook.com>
2019-07-21 14:00:18 +0800

    Update.

--- einstein-macros.h
+++ einstein-macros.h
@@ -1,7 +1,16 @@
 #ifndef EINSTEIN_MACROS_H
 #define EINSTEIN_MACROS_H
 
+#ifdef EINSTEIN_GEN
+
 #define SL_GENERATE [[clang::annotate("SL-generate")]]
 #define SL_NON_POD [[clang::annotate("SL-non-pod")]]
+
+#else // EINSTEIN_GEN
+
+#define SL_GENERATE
+#define SL_NON_POD
+
+#endif // EINSTEIN_GEN
 
 #endif // EINSTEIN_MACROS_H
